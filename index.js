0a7812a479fc722e12b654c984e5b1e999b78af9
joe welch <jwelch19@outlook.com>
2022-03-08 22:07:16 -0500

    commit tree 36b4123dc231cda5e367d8b8647558cabc839a3d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+
+function saturdayFun(activity) {
+   if (activity){
+    return `This Saturday, I want to ${activity}!`;
+   }
+    else {
+    return 'This Saturday, I want to roller-skate!';
+    }
+}
+saturdayFun(activity);
+saturdayFun('bathe my dog');
+
+function mondayWork(activity){
+    if (activity){
+        return `This Monday, I will ${activity}.`;
+    }
+    else {
+        return 'This Monday, I will go to the office.';
+    }
+}
+mondayWork(activity);
+mondayWork("work from home");
+
+function wrapAdjective(flair){
+    return function(name) {
+        return `You are ${flair}${name}${flair}!`;
+    }
+}
+
+wrapAdjective('*')('special');
+wrapAdjective('||')('special');
